6d43dd95ad10a0d9a3666272a3bf13cc7f670a9f
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-01-19 20:29:08 +0700

    update read&write file synchronous

--- writeFile.js
+++ writeFile.js
@@ -2,10 +2,10 @@
 const fs = require('fs')
 
 //อ่านไฟล์ input.txt
-const data = fs.readFileSync('file/sampleFile.txt','utf-8')
+const data = fs.readFileSync('file/input.txt','utf-8')
 console.log(data)
 
 //write file
 const   outputText  = `Hello Node.js\n${data}\nไฟล์ถูกเขียนเมื่อ ${new Date()}`
-fs.writeFileSync("file/sampleFile.txt",outputText)
+fs.writeFileSync("file/output.txt",outputText)
 console.log("เขียนไฟล์เรียบร้อย")
