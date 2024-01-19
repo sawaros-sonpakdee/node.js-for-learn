83a416c45367103cdf651056e3be1016928f2d50
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-01-19 19:31:29 +0700

    lesson modules

---
+++ callmodule.js
@@ -0,0 +1,9 @@
+const mymodules =  require('./modules/myModules.js')
+const number = 500000000
+
+// const getCurrentTime = require('./modules/myModules.js').getCurrentTime
+// const add = require('./modules/myModules.js').add
+
+// console.log(mymodules.getCurrentTime())
+// console.log(mymodules.add(10,20))
+console.log(mymodules.formatNumber(number));
---
+++ modules/myModules.js
@@ -0,0 +1,17 @@
+//ใหบริการเกี่ยวกับการทำงานต่างๆในโปรเจค
+
+function getCurrentTime(){
+    return new Date()
+}
+
+function add(x,y){
+    return x+y
+}
+function formatNumber(num){
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+//ส่งออกคำสั่งไปใช้
+module.exports.getCurrentTime = getCurrentTime
+module.exports.add = add
+module.exports.formatNumber = formatNumber
