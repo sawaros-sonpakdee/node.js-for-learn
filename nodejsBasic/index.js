553f57ed790c91c6b63f048756939e1ecbfe3e1c
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-01-20 21:44:24 +0700

    Update index.js

--- nodejsBasic/index.js
+++ nodejsBasic/index.js
@@ -2,9 +2,29 @@
 
 const http = require('http')
 
-const server = http.createServer(function(req,res){
-    res.writable("Hello Node.js")
-    res.end()
-})
+// //สร้าง server(callback function(request,respon))
+// const server = http.createServer(function(req,res){
+//     res.write("Hello Node.js update")//ถ้ามีการเปลี่ยนแปลงข้อมูล ต้อง restart server
+//     res.end()//บอกจุดสิ้นสุดในการรับ-ส่งข้อมูล
+// })
 
-server.listen(3000)
+// server.listen(3000)
+
+
+
+// http.createServer((req, res) => {
+//     res.write("Hello Node.js update")//ถ้ามีการเปลี่ยนแปลงข้อมูล ต้อง restart server
+//     res.end()//บอกจุดสิ้นสุดในการรับ-ส่งข้อมูล
+// }).listen(8080,()=>{
+//     console.log("start server in port 8080")
+// })
+
+
+
+const server = http.createServer((req, res) => {
+    res.write("<h1>Hello Node.js update</h1>")//ถ้ามีการเปลี่ยนแปลงข้อมูล ต้อง restart server
+    res.end()//บอกจุดสิ้นสุดในการรับ-ส่งข้อมูล
+})
+server.listen(8080,'localhost',()=>{
+    console.log("start server in port 8080")
+})
