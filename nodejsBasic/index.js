937f3123e54512978d8d678ab32e8c2d307f61cf
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-01-24 00:34:35 +0700

    use express.js

--- nodejsBasic/index.js
+++ nodejsBasic/index.js
@@ -1,16 +1,16 @@
 //สร้าง web server
 
-const http = require('http')
-// const pathName = require('path')
-const fs = require('fs') 
-const url = require('url')
+// const http = require('http')
+// // const pathName = require('path')
+// const fs = require('fs') 
+// const url = require('url')
 
-// ระบุตำแหน่งไฟล์  
-const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`,`utf-8`)
+// // ระบุตำแหน่งไฟล์  
+// const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`,`utf-8`)
 
-const productPage1 = fs.readFileSync(`${__dirname}/templates/product1.html`,`utf-8`)
-const productPage2 = fs.readFileSync(`${__dirname}/templates/product2.html`,`utf-8`)
-const productPage3 = fs.readFileSync(`${__dirname}/templates/product3.html`,`utf-8`)
+// const productPage1 = fs.readFileSync(`${__dirname}/templates/product1.html`,`utf-8`)
+// const productPage2 = fs.readFileSync(`${__dirname}/templates/product2.html`,`utf-8`)
+// const productPage3 = fs.readFileSync(`${__dirname}/templates/product3.html`,`utf-8`)
 
 // const server = http.createServer(function(req,res){
 //     res.write("Hello Node.js update")//ถ้ามีการเปลี่ยนแปลงข้อมูล ต้อง restart server
@@ -54,40 +54,51 @@ const productPage3 = fs.readFileSync(`${__dirname}/templates/product3.html`,`utf
 // })
 
 
-//****การ routing  */
-const server = http.createServer((req, res) => {
+// //****การ routing  */
+// const server = http.createServer((req, res) => {
 
-    const {pathname,query} = url.parse(req.url,true)
-    // console.log(url.parse(req.url,true))
-    // const pathName = req.url
-    // console.log("url = ",req)
-    if(pathname === "/" || pathname === "/home"){
-       res.end(indexPage)
-    }
-    else if(pathname === "/product"){
-        console.log(query.id);//ชื่อพารามิเตอร์ และค่าที่อยู่ข้างใน
-        if(query.id === "1"){
-            res.end(productPage1)
-        }
-        else if(query.id === "2"){
-            res.end(productPage2)
-        }
-        else if(query.id === "3"){
-            res.end(productPage3)
-        }
-        else{
-            res.writeHead(404)
-            res.end("<h1>Not Found</h1>")
-        }
-    }
-    else{
-        res.writeHead(404)
-        res.end("<h1>Not Found</h1>")
-    }
+//     const {pathname,query} = url.parse(req.url,true)
+//     // console.log(url.parse(req.url,true))
+//     // const pathName = req.url
+//     // console.log("url = ",req)
+//     if(pathname === "/" || pathname === "/home"){
+//        res.end(indexPage)
+//     }
+//     else if(pathname === "/product"){
+//         console.log(query.id);//ชื่อพารามิเตอร์ และค่าที่อยู่ข้างใน
+//         if(query.id === "1"){
+//             res.end(productPage1)
+//         }
+//         else if(query.id === "2"){
+//             res.end(productPage2)
+//         }
+//         else if(query.id === "3"){
+//             res.end(productPage3)
+//         }
+//         else{
+//             res.writeHead(404)
+//             res.end("<h1>Not Found</h1>")
+//         }
+//     }
+//     else{
+//         res.writeHead(404)
+//         res.end("<h1>Not Found</h1>")
+//     }
     
   
-    res.end()//บอกจุดสิ้นสุดในการรับ-ส่งข้อมูล
+//     res.end()//บอกจุดสิ้นสุดในการรับ-ส่งข้อมูล
+// })
+// server.listen(8080,'localhost',()=>{
+//     console.log("start server in port 8080")
+// })
+
+const express = require('express')
+const app = express()
+
+// (ต้องเขียนก่อน listen)
+app.use((req,res)=>{
+    res.send('<h2>Hello express.js</h2>')
 })
-server.listen(8080,'localhost',()=>{
-    console.log("start server in port 8080")
+app.listen(8080,()=>{
+    console.log('run server on port 8080')
 })
