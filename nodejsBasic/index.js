dfcf1181c7fe2ea46c8ae4dc58138c8f1b4cc41b
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-01-23 23:55:44 +0700

    update Modue url

--- nodejsBasic/index.js
+++ nodejsBasic/index.js
@@ -1,14 +1,16 @@
 //สร้าง web server
 
 const http = require('http')
-const pathName = require('path')
+// const pathName = require('path')
 const fs = require('fs') 
 const url = require('url')
 
 // ระบุตำแหน่งไฟล์  
 const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`,`utf-8`)
 
-const productPage = fs.readFileSync(`${__dirname}/templates/product1.html`,`utf-8`)
+const productPage1 = fs.readFileSync(`${__dirname}/templates/product1.html`,`utf-8`)
+const productPage2 = fs.readFileSync(`${__dirname}/templates/product2.html`,`utf-8`)
+const productPage3 = fs.readFileSync(`${__dirname}/templates/product3.html`,`utf-8`)
 
 // const server = http.createServer(function(req,res){
 //     res.write("Hello Node.js update")//ถ้ามีการเปลี่ยนแปลงข้อมูล ต้อง restart server
@@ -55,14 +57,28 @@ const productPage = fs.readFileSync(`${__dirname}/templates/product1.html`,`utf-
 //****การ routing  */
 const server = http.createServer((req, res) => {
 
-    console.log(url.parse(req.url,true))
-     
+    const {pathname,query} = url.parse(req.url,true)
+    // console.log(url.parse(req.url,true))
+    // const pathName = req.url
     // console.log("url = ",req)
-    if(pathName === "/" || pathName === "/home"){
+    if(pathname === "/" || pathname === "/home"){
        res.end(indexPage)
     }
-    else if(pathName === "/product"){
-        res.end(productPage)
+    else if(pathname === "/product"){
+        console.log(query.id);//ชื่อพารามิเตอร์ และค่าที่อยู่ข้างใน
+        if(query.id === "1"){
+            res.end(productPage1)
+        }
+        else if(query.id === "2"){
+            res.end(productPage2)
+        }
+        else if(query.id === "3"){
+            res.end(productPage3)
+        }
+        else{
+            res.writeHead(404)
+            res.end("<h1>Not Found</h1>")
+        }
     }
     else{
         res.writeHead(404)
