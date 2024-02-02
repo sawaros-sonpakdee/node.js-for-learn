67125ec7fb784a903a69c6583175cd3cc6502ca8
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-02-02 19:08:57 +0700

    static file management

--- nodejsBasic/index.js
+++ nodejsBasic/index.js
@@ -169,7 +169,9 @@
 
 
 const express = require(`express`)
+const path = require(`path`)
 const app = express()
+app.use(express.static(path.join(__dirname,'public')))
 app.listen(8080,()=>{
     console.log("รัน server ที่ port 8080")
 })
