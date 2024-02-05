a38ff4035526b796b22eadb640d5211917c74220
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-02-06 01:38:26 +0700

    move and rename file
    c

--- nodejsBasic/routes/myRouter.js
+++ nodejsBasic/routes/myRouter.js
@@ -37,16 +37,31 @@
 // module.exports = router
 
 
+// const express = require('express')
+// const router = express.Router()
+
+// router.get('/',(req,res)=>{
+//     const name_data = "send propperty data"
+//     // res.render('index.ejs',{data:name,age:18})
+
+//     // const products = ["เสื้อ","พัดลม","หูฟัง","คีย์บอร์ด"]
+//     // res.render('index',{products:products})
+
+//     const products = [ 
+//         {name:"เสื้อผ้า",price:500,image:"images/products/product1.png"},
+//         {name:"เสื้อ",price:200,image:"images/products/product2.png"},
+//         {name:"พัดลม",price:30000,image:"images/products/product3.png"}
+//     ]
+//     res.render('index',{products:products})  
+    
+// })
+// module.exports = router
+
 const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
     const name_data = "send propperty data"
-    // res.render('index.ejs',{data:name,age:18})
-
-    // const products = ["เสื้อ","พัดลม","หูฟัง","คีย์บอร์ด"]
-    // res.render('index',{products:products})
-
     const products = [ 
         {name:"เสื้อผ้า",price:500,image:"images/products/product1.png"},
         {name:"เสื้อ",price:200,image:"images/products/product2.png"},
@@ -55,4 +70,12 @@ router.get('/',(req,res)=>{
     res.render('index',{products:products})  
     
 })
+router.get('/addForm',(req,res)=>{
+    res.render('form')
+})
+
+router.get('/manage',(req,res)=>{
+    res.render('manage')
+})
+
 module.exports = router
