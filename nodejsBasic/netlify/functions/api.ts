a13e647f8223328d8cdad9ad3d8c267baab56cea
Sawaros Sonpakdee <62640957+sawaros-sonpakdee@users.noreply.github.com>
2024-02-27 17:37:16 +0700

    update route of netlify

--- nodejsBasic/netlify/functions/api.ts
+++ nodejsBasic/netlify/functions/api.ts
@@ -8,6 +8,6 @@ const api = express();
 const router = Router();
 router.get("/app", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+api.use('/netlify/functions/', router);
 
 export const handler = serverless(api);
